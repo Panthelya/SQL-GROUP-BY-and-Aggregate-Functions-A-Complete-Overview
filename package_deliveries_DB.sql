373ff3a9aded5549ce2460a32819ea3f73a09b9a
Panthelya <116551755+Panthelya@users.noreply.github.com>
2024-03-25 16:02:55 +0100

    Add files via upload

---
+++ package_deliveries_DB.sql
@@ -0,0 +1,35 @@
+CREATE TABLE package_deliveries (
+	id INT PRIMARY KEY,
+	date_of_delivery DATE,
+	number_of_packages INT,
+	city_of_delivery VARCHAR(120),
+	country_of_delivery VARCHAR(120)
+	
+);
+INSERT INTO package_deliveries (id, date_of_delivery, number_of_packages, city_of_delivery, country_of_delivery) VALUES (1, '2024_03_01', 49, 'Krakow', 'Poland');
+INSERT INTO package_deliveries (id, date_of_delivery, number_of_packages, city_of_delivery, country_of_delivery) VALUES (2, '2024_03_01', 528, 'Warsaw', 'Poland');
+INSERT INTO package_deliveries (id, date_of_delivery, number_of_packages, city_of_delivery, country_of_delivery) VALUES (3, '2024_03_01', 158, 'Gdansk', 'Poland');
+INSERT INTO package_deliveries (id, date_of_delivery, number_of_packages, city_of_delivery, country_of_delivery) VALUES (4, '2024_03_01', 28, 'Rijeka', 'Croatia');
+INSERT INTO package_deliveries (id, date_of_delivery, number_of_packages, city_of_delivery, country_of_delivery) VALUES (5, '2024_03_01', 97, 'Split', 'Croatia');
+INSERT INTO package_deliveries (id, date_of_delivery, number_of_packages, city_of_delivery, country_of_delivery) VALUES (6, '2024_03_01', 64, 'Zagreb', 'Croatia');
+INSERT INTO package_deliveries (id, date_of_delivery, number_of_packages, city_of_delivery, country_of_delivery) VALUES (7, '2024_03_01', 1008, 'Istanbul', 'Turkey');
+INSERT INTO package_deliveries (id, date_of_delivery, number_of_packages, city_of_delivery, country_of_delivery) VALUES (8, '2024_03_01', 514, 'Ankara', 'Turkey');
+INSERT INTO package_deliveries (id, date_of_delivery, number_of_packages, city_of_delivery, country_of_delivery) VALUES (9, '2024_03_01', 284, 'Izmir', 'Turkey');
+INSERT INTO package_deliveries (id, date_of_delivery, number_of_packages, city_of_delivery, country_of_delivery) VALUES (10, '2024_03_02', 145, 'Krakow', 'Poland');
+INSERT INTO package_deliveries (id, date_of_delivery, number_of_packages, city_of_delivery, country_of_delivery) VALUES (11, '2024_03_02', 1089, 'Warsaw', 'Poland');
+INSERT INTO package_deliveries (id, date_of_delivery, number_of_packages, city_of_delivery, country_of_delivery) VALUES (12, '2024_03_02', 13, 'Gdansk', 'Poland');
+INSERT INTO package_deliveries (id, date_of_delivery, number_of_packages, city_of_delivery, country_of_delivery) VALUES (13, '2024_03_02', 287, 'Rijeka', 'Croatia');
+INSERT INTO package_deliveries (id, date_of_delivery, number_of_packages, city_of_delivery, country_of_delivery) VALUES (14, '2024_03_02', 247, 'Split', 'Croatia');
+INSERT INTO package_deliveries (id, date_of_delivery, number_of_packages, city_of_delivery, country_of_delivery) VALUES (15, '2024_03_02', 367, 'Zagreb', 'Croatia');
+INSERT INTO package_deliveries (id, date_of_delivery, number_of_packages, city_of_delivery, country_of_delivery) VALUES (16, '2024_03_02', 4108, 'Istanbul', 'Turkey');
+INSERT INTO package_deliveries (id, date_of_delivery, number_of_packages, city_of_delivery, country_of_delivery) VALUES (17, '2024_03_02', 3478, 'Ankara', 'Turkey');
+INSERT INTO package_deliveries (id, date_of_delivery, number_of_packages, city_of_delivery, country_of_delivery) VALUES (18, '2024_03_02', 1474, 'Izmir', 'Turkey');
+INSERT INTO package_deliveries (id, date_of_delivery, number_of_packages, city_of_delivery, country_of_delivery) VALUES (19, '2024_03_03', 479, 'Krakow', 'Poland');
+INSERT INTO package_deliveries (id, date_of_delivery, number_of_packages, city_of_delivery, country_of_delivery) VALUES (20, '2024_03_03', 741, 'Warsaw', 'Poland');
+INSERT INTO package_deliveries (id, date_of_delivery, number_of_packages, city_of_delivery, country_of_delivery) VALUES (21, '2024_03_03', 541, 'Gdansk', 'Poland');
+INSERT INTO package_deliveries (id, date_of_delivery, number_of_packages, city_of_delivery, country_of_delivery) VALUES (22, '2024_03_03', 312, 'Rijeka', 'Croatia');
+INSERT INTO package_deliveries (id, date_of_delivery, number_of_packages, city_of_delivery, country_of_delivery) VALUES (23, '2024_03_03', 187, 'Split', 'Croatia');
+INSERT INTO package_deliveries (id, date_of_delivery, number_of_packages, city_of_delivery, country_of_delivery) VALUES (24, '2024_03_03', 499, 'Zagreb', 'Croatia');
+INSERT INTO package_deliveries (id, date_of_delivery, number_of_packages, city_of_delivery, country_of_delivery) VALUES (25, '2024_03_03', 8741, 'Istanbul', 'Turkey');
+INSERT INTO package_deliveries (id, date_of_delivery, number_of_packages, city_of_delivery, country_of_delivery) VALUES (26, '2024_03_03', 5147, 'Ankara', 'Turkey');
+INSERT INTO package_deliveries (id, date_of_delivery, number_of_packages, city_of_delivery, country_of_delivery) VALUES (27, '2024_03_03', 4178, 'Izmir', 'Turkey');

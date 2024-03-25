02517b1ec614e1e6c0445218f73cc4c06ccf0f28
Panthelya <116551755+Panthelya@users.noreply.github.com>
2024-03-25 14:38:31 +0100

    Add files via upload

---
+++ albums_DB.sql
@@ -0,0 +1,49 @@
+CREATE TABLE albums (
+	id INT PRIMARY KEY,
+	artist VARCHAR(120),
+	album VARCHAR(120),
+	release_year INT,
+	length INTERVAL,
+	label VARCHAR(120)
+	
+);
+INSERT INTO albums (id, artist, album, release_year, length, label) VALUES (1, 'Grant Green', 'The Latin Bit', 1963, '38M 56S', 'Blue Note Records');
+INSERT INTO albums (id, artist, album, release_year, length, label) VALUES (2, 'AC/DC', 'High Voltage', 1976, '44M 23S', 'Atlantic Records');
+INSERT INTO albums (id, artist, album, release_year, length, label) VALUES (3, 'Brother Jack McDuff', 'To Seek a New Home', 1970, '33M 12S', 'Blue Note Records');
+INSERT INTO albums (id, artist, album, release_year, length, label) VALUES (4, 'Grant Green', 'Green Is Beautiful', 1970, '37M 33S', 'Blue Note Records');
+INSERT INTO albums (id, artist, album, release_year, length, label) VALUES (5, 'Wayne Shorter', 'Moto Grosso Feio', 1974, '42M 22S', 'Blue Note Records');
+INSERT INTO albums (id, artist, album, release_year, length, label) VALUES (6, 'Albert King', 'The Big Blues', 1962, '34M 49S', 'Stax Records');
+INSERT INTO albums (id, artist, album, release_year, length, label) VALUES (7, 'Wayne Shorter', 'Adam''s Apple', 1967, '41M 08S', 'Blue Note Records');
+INSERT INTO albums (id, artist, album, release_year, length, label) VALUES (8, 'Otis Redding', 'Love Man', 1969, '31M 30S', 'Stax Records');
+INSERT INTO albums (id, artist, album, release_year, length, label) VALUES (9, 'Led Zeppelin', 'Led Zeppelin', 1969, '44M 45S', 'Atlantic Records');
+INSERT INTO albums (id, artist, album, release_year, length, label) VALUES (10, 'Isaac Hayes', 'Shaft', 1971, '69M 29S', 'Stax Records');
+INSERT INTO albums (id, artist, album, release_year, length, label) VALUES (11, 'Aretha Franklin', 'I Never Loved a Man the Way I Love You', 1967, '32M 51S', 'Atlantic Records');
+INSERT INTO albums (id, artist, album, release_year, length, label) VALUES (12, 'AC/DC', 'Dirty Deeds Done Dirt Cheap', 1976, '39M 59S', 'Atlantic Records');
+INSERT INTO albums (id, artist, album, release_year, length, label) VALUES (13, 'Wayne Shorter', 'Schizophrenia', 1969, '36M 37S', 'Blue Note Records');
+INSERT INTO albums (id, artist, album, release_year, length, label) VALUES (14, 'Albert King', 'I''ll Play the Blues for You', 1972, '39M 44S', 'Stax Records');
+INSERT INTO albums (id, artist, album, release_year, length, label) VALUES (15, 'John Prine', 'Diamonds in the Rough', 1972, '37M 50S', 'Atlantic Records');
+INSERT INTO albums (id, artist, album, release_year, length, label) VALUES (16, 'John Prine', 'John Prine', 1971, '44M 07S', 'Atlantic Records');
+INSERT INTO albums (id, artist, album, release_year, length, label) VALUES (17, 'Otis Redding', 'Otis Blue/Otis Redding Sings Soul', 1965, '32M 22S', 'Stax Records');
+INSERT INTO albums (id, artist, album, release_year, length, label) VALUES (18, 'Led Zeppelin', 'Houses of the Holy', 1973, '40M 57S', 'Atlantic Records');
+INSERT INTO albums (id, artist, album, release_year, length, label) VALUES (19, 'Aretha Franklin', 'Aretha Now', 1968, '29M 30S', 'Atlantic Records');
+INSERT INTO albums (id, artist, album, release_year, length, label) VALUES (20, 'Grant Green', 'Alive!', 1970, '36M 45S', 'Blue Note Records');
+INSERT INTO albums (id, artist, album, release_year, length, label) VALUES (21, 'Brother Jack McDuff', 'Moon Rappin''', 1969, '46M 00S', 'Blue Note Records');
+INSERT INTO albums (id, artist, album, release_year, length, label) VALUES (22, 'Albert King', 'Years Gone By', 1969, '33M 36S', 'Stax Records');
+INSERT INTO albums (id, artist, album, release_year, length, label) VALUES (23, 'Grant Green', 'Goin'' West', 1969, '34M 12S', 'Blue Note Records');
+INSERT INTO albums (id, artist, album, release_year, length, label) VALUES (24, 'Isaac Hayes', 'Live at the Sahara Tahoe', 1973, '103M 10S', 'Stax Records');
+INSERT INTO albums (id, artist, album, release_year, length, label) VALUES (25, 'Isaac Hayes', '…To Be Continued', 1970, '42M 03S', 'Stax Records');
+INSERT INTO albums (id, artist, album, release_year, length, label) VALUES (26, 'Isaac Hayes', 'The Isaac Hayes Movement', 1970, '36M 18S', 'Stax Records');
+INSERT INTO albums (id, artist, album, release_year, length, label) VALUES (27, 'Otis Redding', 'Pain in My Heart', 1964, '30M 17S', 'Stax Records');
+INSERT INTO albums (id, artist, album, release_year, length, label) VALUES (28, 'Led Zeppelin', 'Led Zeppelin IV', 1971, '42M 37S', 'Atlantic Records');
+INSERT INTO albums (id, artist, album, release_year, length, label) VALUES (29, 'Led Zeppelin', 'Led Zeppelin III', 1970, '43M 04S', 'Atlantic Records');
+INSERT INTO albums (id, artist, album, release_year, length, label) VALUES (30, 'Brother Jack McDuff', 'Down Home Style', 1969, '38M 59S', 'Blue Note Records');
+INSERT INTO albums (id, artist, album, release_year, length, label) VALUES (31, 'Aretha Franklin', 'Young, Gifted and Black', 1972, '44M 46S', 'Atlantic Records');
+INSERT INTO albums (id, artist, album, release_year, length, label) VALUES (32, 'Isaac Hayes', 'Black Moses', 1971, '93M 38S', 'Stax Records');
+INSERT INTO albums (id, artist, album, release_year, length, label) VALUES (33, 'Led Zeppelin', 'Led Zeppelin II', 1969, '40M 44S', 'Atlantic Records');
+INSERT INTO albums (id, artist, album, release_year, length, label) VALUES (34, 'Isaac Hayes', 'Hot Buttered Soul', 1969, '45M 24S', 'Stax Records');
+INSERT INTO albums (id, artist, album, release_year, length, label) VALUES (35, 'AC/DC', 'Let There Be Rock', 1977, '41M 01S', 'Atlantic Records');
+INSERT INTO albums (id, artist, album, release_year, length, label) VALUES (36, 'Wayne Shorter', 'Super Nova', 1969, '37M 55S', 'Blue Note Records');
+INSERT INTO albums (id, artist, album, release_year, length, label) VALUES (37, 'Albert King', 'I Wanna Get Funky', 1974, '46M 00S', 'Stax Records');
+INSERT INTO albums (id, artist, album, release_year, length, label) VALUES (38, 'Albert King', 'Born Under a Bad Sign', 1967, '34M 19S', 'Stax Records');
+INSERT INTO albums (id, artist, album, release_year, length, label) VALUES (39, 'Grant Green', 'Idle Moments', 1965, '42M 45S', 'Blue Note Records');
+
